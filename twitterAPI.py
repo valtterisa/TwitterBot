0afa926d278c075ae160f306322dec0448365783
Valtteri Savonen <savonen.emppu@gmail.com>
2022-10-24 01:08:11 +0300

    New version

--- twitterAPI.py
+++ twitterAPI.py
@@ -1,4 +1,6 @@
+from time import time
 import tweepy
+import datetime
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
@@ -8,7 +10,7 @@ from bs4 import BeautifulSoup
 # reading keys from file
 
 # open keys.txt file
-file = open("/keys.txt","r")
+file = open("keys.txt","r")
 read = file.readlines()
 
 # create new array for elements
@@ -30,6 +32,9 @@ authenticator.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(authenticator, wait_on_rate_limit = True)
 
+# closing file
+file.close()
+
 print("auth OK...")
 #########################################################################################
 
@@ -44,7 +49,7 @@ def get_index_data(url):
     data = []
 
     # read data from internet
-    html = urlopen(url, timeout = 10).read()
+    html = urlopen(url, timeout = 20).read()
     soup = BeautifulSoup(html, "html.parser")
 
     # GET data from specific elements
@@ -72,24 +77,32 @@ def get_index_data(url):
     return data
 
 # Tweet data
-def tweetAll() :
+def tweetAll(arg1,arg2) :
+
+    # add current date and time (day/month/year) 
+    time_now = datetime.datetime.now().strftime("%d/%m/%Y")
 
     # URLs for wanted indexes
 
     # url0 = "https://www.google.com/finance/quote/DAX:INDEXDB"
-    url1 =  "https://www.google.com/finance/quote/OMXHPI:INDEXNASDAQ"
-    # url3 =
-    # url4 =
-    # url5 =
-    # url6 =
-    # url7 =
+    url1 =  "https://www.google.com/finance/quote/OMXHPI:INDEXNASDAQ" # OMX Helsinki PI
+    url2 = "https://www.google.com/finance/quote/FNFIEURPI:INDEXNASDAQ" # First North Finland EUR PI
+    url3 = "https://www.google.com/finance/quote/OMXH25:INDEXNASDAQ" # OMX Helsinki 25
 
     # dax_data = get_index_data(url0)[0] + ": " + get_index_data(url0)[1] + " " + get_index_data(url0)[2] 
-    omxhpi_data = get_index_data(url1)[0] + ": " + get_index_data(url1)[1] + " " + get_index_data(url1)[2]
+    omxhpi_data = get_index_data(url1)[0] + ": " + get_index_data(url1)[1] + " (" + get_index_data(url1)[2] + "%)"
+    fnfpi_data = get_index_data(url2)[0] + ": " + get_index_data(url2)[1] + " (" + get_index_data(url2)[2] + "%)"
+    omxh25_data = get_index_data(url3)[0] + ": " + get_index_data(url3)[1] + " (" + get_index_data(url3)[2] + "%)"
 
     # Posting tweet
     print("updating twitter status...")
-    api.update_status(omxhpi_data + "\n" + "#talous")
+
+    api.update_status("Helsingin pörssi " + time_now + "\n" +
+                    omxhpi_data + "\n" +
+                    omxh25_data + "\n" +
+                    fnfpi_data + "\n" +
+                    "#talous")
+
     print("Status updated!")
 
 tweetAll()
