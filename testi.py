ce54600c62d0f9ebdcf3ee38e518a4fc9ec99192
Valtteri <savonen.emppu@gmail.com>
2022-11-24 22:49:33 +0200

    testifilu printille

---
+++ testi.py
@@ -0,0 +1,8 @@
+print(
+                    "Helsingin pörssi" + " - " +  "24/11/2022" + "\n" + "\n" +
+                    "OMX Helsinki PI: 11,040.32 (+1.12%)"    + "\n" +
+                    "OMX Helsinki 25: 4,925.13 (+0.99%)"     + "\n" +
+                    "First North Finland: 1,705.86 (+0.08%)" + "\n" + "\n" +
+                    "#talous" + " #sijoittaminen" + "\n")
+
+                
